b2af2719c71331f413b443b1808fe6ec15cc91a7
kiba518 <liujin_hzzh@126.com>
2019-08-12 17:13:59 +0800

    wpf OpenCV

---
+++ ImageHelper.cs
@@ -0,0 +1,392 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace WpfOpenCV
+{
+    public static class ImageHelper
+    {
+        public static byte[] GetImage(string path)
+        {
+            byte[] ret = null;
+            var mem = new System.IO.MemoryStream(System.IO.File.ReadAllBytes(path)); 
+            ret = mem.ToArray();
+            mem.Close();
+            return ret;
+        }
+
+        /// <summary>
+        /// Convert Image to Byte[]
+        /// </summary>
+        /// <param name="image"></param>
+        /// <returns></returns>
+        public static byte[] ImageToBytes(Image image)
+        {
+            ImageFormat format = image.RawFormat;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                if (format.Equals(ImageFormat.Jpeg))
+                {
+                    image.Save(ms, ImageFormat.Jpeg);
+                }
+                else if (format.Equals(ImageFormat.Png))
+                {
+                    image.Save(ms, ImageFormat.Png);
+                }
+                else if (format.Equals(ImageFormat.Bmp))
+                {
+                    image.Save(ms, ImageFormat.Bmp);
+                }
+                else if (format.Equals(ImageFormat.Gif))
+                {
+                    image.Save(ms, ImageFormat.Gif);
+                }
+                else if (format.Equals(ImageFormat.Icon))
+                {
+                    image.Save(ms, ImageFormat.Icon);
+                }
+                byte[] buffer = new byte[ms.Length];
+                //Image.Save()会改变MemoryStream的Position，需要重新Seek到Begin
+                ms.Seek(0, SeekOrigin.Begin);
+                ms.Read(buffer, 0, buffer.Length);
+                return buffer;
+            }
+        }
+
+        /// <summary>
+        /// Convert Byte[] to Image
+        /// </summary>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static Image BytesToImage(byte[] buffer)
+        {
+            MemoryStream ms = new MemoryStream(buffer);
+            Image image = System.Drawing.Image.FromStream(ms);
+            return image;
+        }
+
+
+        public static BitmapImage BytesToBitmapImage(byte[] byteArray)
+        {
+            BitmapImage bmp = null;
+
+            try
+            {
+                bmp = new BitmapImage();
+                bmp.BeginInit();
+                bmp.StreamSource = new MemoryStream(byteArray);
+                bmp.EndInit();
+            }
+            catch
+            {
+                bmp = null;
+            } 
+            return bmp;
+        }
+        public static byte[] BitmapImageToBytes(BitmapImage bmp)
+        {
+            byte[] byteArray = null;
+
+            try
+            {
+                Stream sMarket = bmp.StreamSource;
+
+                if (sMarket != null && sMarket.Length > 0)
+                {
+                    //很重要，因为Position经常位于Stream的末尾，导致下面读取到的长度为0。 
+                    sMarket.Position = 0;
+
+                    using (BinaryReader br = new BinaryReader(sMarket))
+                    {
+                        byteArray = br.ReadBytes((int)sMarket.Length);
+                    }
+                }
+            }
+            catch
+            {
+                //other exception handling 
+            }
+
+            return byteArray;
+        }
+        /// <summary>
+        /// Convert Byte[] to a picture and Store it in file
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="buffer"></param>
+        /// <returns></returns>
+        public static string CreateImageFromBytes(string fileName, byte[] buffer)
+        {
+            string file = fileName;
+            Image image = BytesToImage(buffer);
+            ImageFormat format = image.RawFormat;
+            if (format.Equals(ImageFormat.Jpeg))
+            {
+                file += ".jpeg";
+            }
+            else if (format.Equals(ImageFormat.Png))
+            {
+                file += ".png";
+            }
+            else if (format.Equals(ImageFormat.Bmp))
+            {
+                file += ".bmp";
+            }
+            else if (format.Equals(ImageFormat.Gif))
+            {
+                file += ".gif";
+            }
+            else if (format.Equals(ImageFormat.Icon))
+            {
+                file += ".icon";
+            }
+            System.IO.FileInfo info = new System.IO.FileInfo(file);
+            System.IO.Directory.CreateDirectory(info.Directory.FullName);
+            File.WriteAllBytes(file, buffer);
+            return file;
+        }
+         
+
+        private static Size NewSize(int maxWidth, int maxHeight, int width, int height)
+        {
+            double w = 0.0;
+            double h = 0.0;
+            double sw = Convert.ToDouble(width);
+            double sh = Convert.ToDouble(height); double mw = Convert.ToDouble(maxWidth);
+            double mh = Convert.ToDouble(maxHeight); if (sw < mw && sh < mh)
+            {
+                w = sw;
+                h = sh;
+            }
+            else if ((sw / sh) > (mw / mh))
+            {
+                w = maxWidth;
+                h = (w * sh) / sw;
+            }
+            else
+            {
+                h = maxHeight;
+                w = (h * sw) / sh;
+            }
+            return new Size(Convert.ToInt32(w), Convert.ToInt32(h));
+        }
+        /// <summary>
+        /// 缩略图方法 调用方法 ImageHelper.CreateSmallImage(@"F:\bot\4.png",@"F:\bot\11111.jpg",100,100); 会替换已存的图片
+        /// </summary>
+        /// <param name="fileName">源路径</param>
+        /// <param name="newFile">新图片路径</param>
+        /// <param name="maxHeight">最大高度</param>
+        /// <param name="maxWidth">最大宽度</param> 
+        public static void CreateSmallImage(string fileName, string newFile, int maxHeight, int maxWidth)
+        {
+            System.Drawing.Image img = System.Drawing.Image.FromFile(fileName);
+            System.Drawing.Imaging.ImageFormat thisFormat = img.RawFormat;
+            Size newSize = NewSize(maxWidth, maxHeight, img.Width, img.Height);
+            Bitmap outBmp = new Bitmap(newSize.Width, newSize.Height);
+            Graphics g = Graphics.FromImage(outBmp);
+            // 设置画布的描绘质量
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.SmoothingMode = SmoothingMode.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.DrawImage(img, new Rectangle(0, 0, newSize.Width, newSize.Height),
+            0, 0, img.Width, img.Height, GraphicsUnit.Pixel);
+            g.Dispose();
+            // 以下代码为保存图片时,设置压缩质量
+            EncoderParameters encoderParams = new EncoderParameters();
+            long[] quality = new long[1];
+            quality[0] = 100;
+            EncoderParameter encoderParam = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, quality);
+            encoderParams.Param[0] = encoderParam;
+            //获得包含有关内置图像编码解码器的信息的ImageCodecInfo 对象.
+            ImageCodecInfo[] arrayICI = ImageCodecInfo.GetImageEncoders();
+            ImageCodecInfo jpegICI = null;
+            for (int x = 0;
+            x < arrayICI.Length;
+            x++)
+            {
+                if (arrayICI[x].FormatDescription.Equals("JPEG"))
+                {
+                    jpegICI = arrayICI[x];
+                    //设置JPEG编码
+                    break;
+                }
+            }
+            if (jpegICI != null)
+            {
+                outBmp.Save(newFile, jpegICI, encoderParams);
+            }
+            else
+            {
+                outBmp.Save(newFile,
+                thisFormat);
+            }
+            img.Dispose();
+            outBmp.Dispose();
+        }
+        /// <summary>
+        /// 图片加水印文字
+        /// </summary>
+        /// <param name="oldpath">旧图片地址</param>
+        /// <param name="text">水印文字</param>
+        /// <param name="newpath">新图片地址</param>
+        /// <param name="Alpha">透明度0-255 透明度越大越淡</param>
+        /// <param name="fontsize">字体大小</param>
+        public static void AddWaterText(string oldpath, string text, string newpath, int Alpha, int fontsize)
+        {
+            try
+            {
+                text = text + "版权所有";
+                FileStream fs = new FileStream(oldpath, FileMode.Open);
+                BinaryReader br = new BinaryReader(fs);
+                byte[] bytes = br.ReadBytes((int)fs.Length);
+                br.Close();
+                fs.Close();
+                MemoryStream ms = new MemoryStream(bytes);
+
+                System.Drawing.Image imgPhoto = System.Drawing.Image.FromStream(ms);
+                int imgPhotoWidth = imgPhoto.Width;
+                int imgPhotoHeight = imgPhoto.Height;
+
+                Bitmap bmPhoto = new Bitmap(imgPhotoWidth, imgPhotoHeight, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+                bmPhoto.SetResolution(72, 72);
+                Graphics gbmPhoto = Graphics.FromImage(bmPhoto);
+                //gif背景色
+                gbmPhoto.Clear(Color.FromName("white"));
+                gbmPhoto.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                gbmPhoto.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                gbmPhoto.DrawImage(imgPhoto, new Rectangle(0, 0, imgPhotoWidth, imgPhotoHeight), 0, 0, imgPhotoWidth, imgPhotoHeight, GraphicsUnit.Pixel);
+                System.Drawing.Font font = null;
+                System.Drawing.SizeF crSize = new SizeF();
+                font = new Font("宋体", fontsize, FontStyle.Bold);
+                //测量指定区域
+                crSize = gbmPhoto.MeasureString(text, font);
+                float y = imgPhotoHeight - crSize.Height;
+                float x = imgPhotoWidth - crSize.Width;
+                System.Drawing.StringFormat StrFormat = new System.Drawing.StringFormat();
+                StrFormat.Alignment = System.Drawing.StringAlignment.Center;
+
+                //画两次制造透明效果
+                System.Drawing.SolidBrush semiTransBrush2 = new System.Drawing.SolidBrush(Color.FromArgb(Alpha, 56, 56, 56));
+                gbmPhoto.DrawString(text, font, semiTransBrush2, x + 1, y + 1);
+
+                System.Drawing.SolidBrush semiTransBrush = new System.Drawing.SolidBrush(Color.FromArgb(Alpha, 176, 176, 176));
+                gbmPhoto.DrawString(text, font, semiTransBrush, x, y);
+                bmPhoto.Save(newpath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                gbmPhoto.Dispose();
+                imgPhoto.Dispose();
+                bmPhoto.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+        /// <summary>
+        /// 图片加水印文字
+        /// </summary>
+        /// <param name="oldpath">旧图片地址</param>
+        /// <param name="text">水印文字</param>
+        /// <param name="newpath">新图片地址</param>
+        /// <param name="Alpha">透明度0-255 透明度越大越淡</param>
+        /// <param name="fontsize">字体大小</param>
+        public static void AddWaterImg(string oldpath, string newpath, string waterImgpath)
+        {
+            try
+            {
+
+                FileStream fs = new FileStream(oldpath, FileMode.Open);
+                BinaryReader br = new BinaryReader(fs);
+                byte[] bytes = br.ReadBytes((int)fs.Length);
+                br.Close();
+                fs.Close();
+                MemoryStream ms = new MemoryStream(bytes);
+
+                System.Drawing.Image imgPhoto = System.Drawing.Image.FromStream(ms);
+                int imgPhotoWidth = imgPhoto.Width;
+                int imgPhotoHeight = imgPhoto.Height;
+
+                Bitmap bmPhoto = new Bitmap(imgPhotoWidth, imgPhotoHeight, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+                bmPhoto.SetResolution(72, 72);
+                Graphics gbmPhoto = Graphics.FromImage(bmPhoto);
+                //gif背景色
+                gbmPhoto.Clear(Color.FromName("white"));
+                gbmPhoto.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.High;
+                gbmPhoto.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                gbmPhoto.DrawImage(imgPhoto, new Rectangle(0, 0, imgPhotoWidth, imgPhotoHeight), 0, 0, imgPhotoWidth, imgPhotoHeight, GraphicsUnit.Pixel);
+
+
+                FileStream fswaterImg = new FileStream(waterImgpath, FileMode.Open);
+                BinaryReader brwaterImg = new BinaryReader(fswaterImg);
+                byte[] byteswaterImg = brwaterImg.ReadBytes((int)fswaterImg.Length);
+                brwaterImg.Close();
+                fswaterImg.Close();
+                MemoryStream mswaterImg = new MemoryStream(byteswaterImg);
+
+                System.Drawing.Image imgPhotowaterImg = System.Drawing.Image.FromStream(mswaterImg);
+                PointF pf = new PointF();
+                pf.X = imgPhotoWidth - imgPhotowaterImg.Width;
+                pf.Y = imgPhotoHeight - imgPhotowaterImg.Height;
+                ;
+                gbmPhoto.DrawImage(imgPhotowaterImg, pf);
+
+                bmPhoto.Save(newpath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                gbmPhoto.Dispose();
+                imgPhoto.Dispose();
+                bmPhoto.Dispose();
+                imgPhotowaterImg.Dispose();
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+        /// 图片裁剪，生成新图，保存在同一目录下,名字加_new，格式1.png  新图1_new.png
+        /// </summary>
+        /// <param name="picPath">要修改图片完整路径</param>
+        /// <param name="x">修改起点x坐标</param>
+        /// <param name="y">修改起点y坐标</param>
+        /// <param name="width">新图宽度</param>
+        /// <param name="height">新图高度</param>
+        public static void CutPicture(string oldpath, string newpath)
+        {
+            System.Drawing.Image img = System.Drawing.Image.FromFile(oldpath);
+            double rate = 180.00 / 250.00;
+            int width = (int)(rate * img.Height);
+            int x = (img.Width - width) / 2;
+            int y = 0;
+            //System.Drawing.Image img = System.Drawing.Image.FromStream(new System.IO.MemoryStream(System.IO.File.ReadAllBytes(oldpath)));
+            //定义截取矩形
+            System.Drawing.Rectangle cropArea = new System.Drawing.Rectangle(x, y, width, img.Height);
+            //要截取的区域大小
+            //加载图片
+           
+            //判断超出的位置否
+            if ((img.Width < x + width))
+            {
+                img.Dispose();
+                throw new  Exception("裁剪尺寸超出原有尺寸！");  
+            }
+            //定义Bitmap对象
+            System.Drawing.Bitmap bmpImage = new System.Drawing.Bitmap(img);
+            //进行裁剪
+            System.Drawing.Bitmap bmpCrop = bmpImage.Clone(cropArea, bmpImage.PixelFormat);
+            //保存成新文件
+            bmpCrop.Save(newpath);
+            //释放对象
+            img.Dispose();
+            bmpCrop.Dispose();
+        }
+    }
+
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+namespace WpfOpenCV
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : System.Windows.Window
+    {
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        //正常
+        private void BtnChange0_Click(object sender, RoutedEventArgs e)
+        {
+            using (var src = new Mat(@"..\..\Images\ocv02.jpg", ImreadModes.Unchanged))
+            {
+                var bmp = src.ToBitmap();//需要引用OpenCvSharp.Extensions，才能使用ToBitmap
+                var returnSource = Imaging.CreateBitmapSourceFromHBitmap(bmp.GetHbitmap(), IntPtr.Zero, Int32Rect.Empty, BitmapSizeOptions.FromEmptyOptions()); 
+                imgOutput.Source = returnSource;
+            }
+        }
+
+        //蓝红颜色互换
+        private void BtnChange1_Click(object sender, RoutedEventArgs e)
+        {
+            Mat mat = new Mat(@"..\..\Images\ocv02.jpg", ImreadModes.Unchanged);
+            //Mat mat = new Mat(new OpenCvSharp.Size(128, 128), MatType.CV_8U, Scalar.All(255));
+            for (var y = 0; y < mat.Height; y++)
+            {
+                for (var x = 0; x < mat.Width; x++)
+                {
+                    Vec3b color = mat.Get<Vec3b>(y, x);
+                    var temp = color.Item0;
+                    color.Item0 = color.Item2; // B <- R
+                    color.Item2 = temp;        // R <- B
+                    mat.Set(y, x, color);
+                }
+            }
+            var mem = mat.ToMemoryStream();
+            BitmapImage bmp = new BitmapImage();
+            bmp.BeginInit();
+            bmp.StreamSource = mem;
+            bmp.EndInit();
+            imgOutput.Source = bmp;
+            mat.Dispose();/// 该方法在mat里被重写了，可以释放资源，可以放心调用
+        }
+
+        //图片灰显
+        private void BtnChange2_Click(object sender, RoutedEventArgs e)
+        {
+            using (var src = new Mat(@"..\..\Images\ocv02.jpg", ImreadModes.Grayscale))
+            {
+                var mem = src.ToMemoryStream();
+                BitmapImage bmp = new BitmapImage();
+                bmp.BeginInit();
+                bmp.StreamSource = mem;
+                bmp.EndInit();
+                imgOutput.Source = bmp; 
+            }
+        }
+
+        //2倍缩小读取图像
+        private void BtnChange3_Click(object sender, RoutedEventArgs e)
+        {
+            using (var src = new Mat(@"..\..\images\ocv02.jpg", ImreadModes.ReducedGrayscale2))
+            {
+                var mem = src.ToMemoryStream();
+                BitmapImage bmp = new BitmapImage();
+                bmp.BeginInit();
+                bmp.StreamSource = mem;
+                bmp.EndInit();
+                imgOutput.Source = bmp;
+                //using (var srcCopy = new Mat())
+                //{
+                //    src.CopyTo(srcCopy);
+                //    Cv2.CvtColor(srcCopy, srcCopy, ColorConversionCodes.BayerGR2GRAY);//copy成灰色，二倍缩小
+                //    var mem2 = srcCopy.ToMemoryStream();
+                //    BitmapImage bmp2 = new BitmapImage();
+                //    bmp2.BeginInit();
+                //    bmp2.StreamSource = mem2;
+                //    bmp2.EndInit();
+                //    imgOutput.Source = bmp2;
+                //}
+            }
+        }
+
+        //腐蚀
+        private void BtnChange4_Click(object sender, RoutedEventArgs e)
+        {
+            using (var src = new Mat(@"..\..\Images\ocv02.jpg", ImreadModes.AnyDepth | ImreadModes.AnyColor))
+            {
+                Cv2.Erode(src, src, new Mat());
+                var mem = src.ToMemoryStream();
+                BitmapImage bmp = new BitmapImage();
+                bmp.BeginInit();
+                bmp.StreamSource = mem;
+                bmp.EndInit();
+                imgOutput.Source = bmp; 
+            }
+        }
+
+        //膨胀
+        private void BtnChange5_Click(object sender, RoutedEventArgs e)
+        {
+            using (var src = new Mat(@"..\..\Images\ocv02.jpg", ImreadModes.AnyDepth | ImreadModes.AnyColor))
+            {
+                using (var dst = new Mat())//复制以后处理
+                {
+                    Cv2.Dilate(src, dst, new Mat());
+                    var mem = dst.ToMemoryStream();
+                    BitmapImage bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    bmp.StreamSource = mem;
+                    bmp.EndInit();
+                    imgOutput.Source = bmp;
+                }
+            }
+          
+        }
+  
+
+
+        //反转
+        private void BtnChange6_Click(object sender, RoutedEventArgs e)
+        {
+            using (var src = new Mat(@"..\..\Images\ocv02.jpg", ImreadModes.AnyDepth | ImreadModes.AnyColor))
+            {
+                using (var dst = new Mat())//复制以后处理
+                {
+                    Cv2.BitwiseNot(src, dst, new Mat());
+                    var mem = dst.ToMemoryStream();
+                    BitmapImage bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    bmp.StreamSource = mem;
+                    bmp.EndInit();
+                    imgOutput.Source = bmp;
+                }
+            } 
+        }
+
+        //变换顶点
+        private void BtnChange7_Click(object sender, RoutedEventArgs e)
+        { 
+            using (var src = new Mat(@"..\..\Images\ocv02.jpg", ImreadModes.AnyDepth | ImreadModes.AnyColor))
+            {
+                using (var dst = new Mat())//复制以后处理
+                {   
+                    //设置原图变换顶点
+                   List< Point2f> AffinePoints0  =new List<Point2f>() { new Point2f(100, 50), new Point2f(100, 390), new Point2f(600, 50) };
+                    //设置目标图像变换顶点
+                    List<Point2f> AffinePoints1 = new List<Point2f>() { new Point2f(200, 100), new Point2f(200, 330), new Point2f(500, 50) };
+                    //计算变换矩阵
+                    Mat Trans =Cv2.GetAffineTransform(AffinePoints0, AffinePoints1);
+                    //矩阵仿射变换
+                    Cv2.WarpAffine(src, dst, Trans,new OpenCvSharp.Size() { Height= src.Cols, Width= src.Rows }); 
+                    var mem = dst.ToMemoryStream();
+                    BitmapImage bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    bmp.StreamSource = mem;
+                    bmp.EndInit();
+                    imgOutput.Source = bmp;
+                }
+            }
+            
+        }
+        //     blur(edges, edges, Size(7, 7));//模糊化  
+        //Canny(edges, edges, 0, 30, 3);//边缘化  
+        //模糊
+        private void BtnChange8_Click(object sender, RoutedEventArgs e)
+        {
+            using (var src = new Mat(@"..\..\Images\ocv02.jpg", ImreadModes.AnyDepth | ImreadModes.AnyColor))
+            {
+                using (var dst = new Mat())//复制以后处理
+                {
+                    Cv2.Blur(src, dst, new OpenCvSharp.Size(7, 7));
+                    var mem = dst.ToMemoryStream();
+                    BitmapImage bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    bmp.StreamSource = mem;
+                    bmp.EndInit();
+                    imgOutput.Source = bmp;
+                }
+            }
+        }
+        //边缘化  
+        private void BtnChange9_Click(object sender, RoutedEventArgs e)
+        {
+            using (var src = new Mat(@"..\..\Images\ocv02.jpg", ImreadModes.AnyDepth | ImreadModes.AnyColor))
+            {
+                using (var dst = new Mat())//复制以后处理
+                {
+                    Cv2.Canny(src, dst, 0, 30, 3);
+                    var mem = dst.ToMemoryStream();
+                    BitmapImage bmp = new BitmapImage();
+                    bmp.BeginInit();
+                    bmp.StreamSource = mem;
+                    bmp.EndInit();
+                    imgOutput.Source = bmp;
+                }
+            }
+        }
+    }
+}
